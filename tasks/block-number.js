76222f7babd990137cb09faa0b6cb4ccaed81217
Maxim <maxim.gaina@monaitech.eu>
2021-04-28 15:44:51 +0200

    some vulnerabilties fixed

--- tasks/block-number.js
+++ tasks/block-number.js
@@ -1,7 +1,7 @@
 task(
     'block-number',
     'Prints the current block number',
-    async (_) => {
+    async () => {
         await ethers.provider.getBlockNumber().then((blockNumber) => {
             console.log('Current block number: ' + blockNumber);
         });
