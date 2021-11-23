84d8f8221a4eacdcf32e2919a617e8f3b23dd6e1
gnmaxim <maximgaina@yandex.ru>
2021-11-23 08:12:14 +0100

    FEAT(contract): added IERC1238 and ERC1238 with tokenURI

---
+++ contracts/ERC1238.sol
@@ -0,0 +1,120 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import {IERC1238} from './IERC1238.sol';
+
+contract ERC1238 is IERC1238 {
+    // Badge's name
+    string private _name;
+
+    // Badge's symbol
+    string private _symbol;
+
+    // Mapping from token ID to owner's address
+    mapping(uint256 => address) private _owners;
+
+    // Mapping from owner's address to token ID
+    mapping(address => uint256) private _tokens;
+
+    // Mapping from token ID to token URI
+    mapping(uint256 => string) private _tokenURIs;
+
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
+    }
+
+    // Returns the badge's name
+    function name() public view virtual override returns (string memory) {
+        return _name;
+    }
+
+    // Returns the badge's symbol
+    function symbol() public view virtual override returns (string memory) {
+        return _symbol;
+    }
+
+    // Returns the token ID owned by `owner`, if it exists, and 0 otherwise
+    function tokenOf(address owner)
+        public
+        view
+        virtual
+        override
+        returns (uint256)
+    {
+        require(owner != address(0), 'Invalid owner at zero address');
+
+        return _tokens[owner];
+    }
+
+    // Returns the owner of a given token ID, reverts if the token does not exist
+    function ownerOf(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (address)
+    {
+        require(tokenId != 0, 'Invalid tokenId value');
+
+        address owner = _owners[tokenId];
+
+        require(owner != address(0), 'Invalid owner at zero address');
+
+        return owner;
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            'ERC721Metadata: URI query for nonexistent token'
+        );
+
+        return _tokenURIs[tokenId];
+    }
+
+    // Checks if a token ID exists
+    function _exists(uint256 tokenId) internal view virtual returns (bool) {
+        return _owners[tokenId] != address(0);
+    }
+
+    // @dev Mints `tokenId` and transfers it to `to`.
+    function _mint(address to, uint256 tokenId, string memory _tokenURI) internal virtual {
+        require(to != address(0), 'Invalid owner at zero address');
+        require(tokenId != 0, 'Token ID cannot be zero');
+        require(!_exists(tokenId), 'Token already minted');
+        require(tokenOf(to) == 0, 'Owner already has a token');
+
+        _tokens[to] = tokenId;
+        _owners[tokenId] = to;
+        _setTokenURI(tokenId, _tokenURI);
+
+        emit Minted(to, tokenId, block.timestamp);
+    }
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI)
+        internal
+        virtual
+    {
+        require(
+            _exists(tokenId),
+            'ERC721Metadata: URI set of nonexistent token'
+        );
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    // @dev Burns `tokenId`.
+    function _burn(uint256 tokenId) internal virtual {
+        address owner = ERC1238.ownerOf(tokenId);
+
+        delete _tokens[owner];
+        delete _owners[tokenId];
+
+        emit Burned(owner, tokenId, block.timestamp);
+    }
+}
---
+++ contracts/IERC1238.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+
+interface IERC1238 {
+    // @dev Emitted when `tokenId` token is minted to `to`, an address.
+    event Minted(
+        address indexed to,
+        uint256 indexed tokenId,
+        uint256 timestamp
+    );
+
+    // @dev Emitted when `tokenId` token is burned.
+    event Burned(
+        address indexed owner,
+        uint256 indexed tokenId,
+        uint256 timestamp
+    );
+
+    // @dev Returns the badge's name
+    function name() external view returns (string memory);
+
+    // @dev Returns the badge's symbol.
+    function symbol() external view returns (string memory);
+
+    // @dev Returns the ID of the token owned by `owner`, if it owns one, and 0 otherwise
+    function tokenOf(address owner) external view returns (uint256);
+
+    // @dev Returns the owner of the `tokenId` token.
+    function ownerOf(uint256 tokenId) external view returns (address);
+}
